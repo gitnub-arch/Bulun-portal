a450cfb2dee089820f66036c4b84450d3e06fe01
Kostenko Oleg <ok609035@gmail.com>
2024-09-17 01:28:42 +0600

    add change pr

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import WeatherAside from "./shared/global_aside/weather_aside/WeatherAside";
-import RoadstatusAside from "./shared/global_aside/roadStatus_asede/RoadstatusAside";
-import FlightsAside from "./shared/global_aside/flights_aside/FlightsAside";
-import Header from "./shared/header/Header";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './shared/header/Header';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <>
