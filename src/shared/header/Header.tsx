f3a9c763ab36750dff2229d83f207e19a2f42bf6
Kostenko Oleg <ok609035@gmail.com>
2024-09-18 00:51:19 +0600

    add change in header and separator

--- src/shared/header/Header.tsx
+++ src/shared/header/Header.tsx
@@ -10,15 +10,15 @@ import { LINKS_ITEM } from "./const";
 import { useState } from "react";
 
 const Header = () => {
-  const [activeLink, setActiveLink] = useState(useState(LINKS_ITEM[1]));
+  const [activeLink, setActiveLink] = useState(LINKS_ITEM[0]);
 
   const handleLinkClick = (link: any) => {
     setActiveLink(link);
   };
 
   return (
-    <div className="mt-3 container m-auto">
-      <div className="flex justify-between">
+    <div className="mt-3">
+      <div className="flex justify-between mx-8">
         <div className="flex items-center">
           <h4 className="mr-2 font-medium text-base text-[#76767A]">Тиксик</h4>
           <ChevronDown className="text-[#B3B3B3] w-[18px] h-[18px]" />
@@ -35,22 +35,29 @@ const Header = () => {
           </span>
         </div>
       </div>
-      <Separator className="mt-[26px] mb-[26px]" />
-      <div className="flex justify-between">
-        <AlignJustifyIcon className="text-[#DADADA]" />
-        {LINKS_ITEM.map((link) => (
-          <a
-            key={link.label}
-            href={link.href}
-            className="text-gray-600 font-medium mr-4"
-            onClick={() => handleLinkClick(link)}
-          >
-            {link.label}
-            {activeLink[0] === link && (
-              <hr className="mt-4 border-[#1875F0] border-[2px]" />
-            )}
-          </a>
-        ))}
+      <Separator className="bg-[#DADADA] mt-[26px] mb-[26px] w-full" />
+      <div className="flex justify-between mx-8">
+        <div>
+          <AlignJustifyIcon className="text-[#DADADA]" />
+        </div>
+        <Separator orientation="vertical" className="bg-[#DADADA] -mt-6" />
+        <div className="flex justify-center w-[90%] gap-9">
+          {LINKS_ITEM.map((link) => (
+            <a
+              key={link.label}
+              href={link.href}
+              className="text-gray-600 font-medium mr-4 p-1"
+              onClick={() => handleLinkClick(link)}
+            >
+              {link.label}
+              {activeLink.label === link.label && (
+                <hr className="mt-4 border-[#1875F0] border-[2px]" />
+              )}
+            </a>
+          ))}
+        </div>
+        <Separator orientation="vertical" className="bg-[#DADADA] -mt-6 " />
+
         <Search className="text-[#999999]" />
       </div>
     </div>
