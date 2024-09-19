55d3505ad79d6cdc9c457926d715bfc8c7eca94f
Narizizu <tobokelovna1n@gmail.com>
2024-09-19 10:50:32 +0600

    changed styles

--- src/shared/footer/Footer.tsx
+++ src/shared/footer/Footer.tsx
@@ -4,16 +4,16 @@ const Footer = () => {
         <footer className="bg-white p-4 text-black">
   <div className="max-w-7xl mx-auto flex justify-between items-center">
     <nav>
-      <ul className="flex space-x-4 gap-6">
-        <li className="fond-bold">Булунский портал</li>
-        <li><a href="#" className="hover:underline">Главная</a></li>
-        <li><a href="#" className="hover:underline">Новости</a></li>
-        <li><a href="#" className="hover:underline">Объявления</a></li>
-        <li><a href="#" className="hover:underline">Афиша</a></li>
-        <li><a href="#" className="hover:underline">Транспорт</a></li>
-        <li><a href="#" className="hover:underline">Форум</a></li>
-        <li><a href="#" className="hover:underline">Администрация</a></li>
-        <li><a href="#" className="hover:underline">Архив</a></li>
+      <ul className="flex space-x-4 gap-4">
+        <li className="text-black font-bold ml-56">Булунский портал</li>
+        <li><a href="#" className="hover:text-black text-[#76767A] ">Новости</a></li>
+        <li><a href="#" className="hover:text-black text-[#76767A]">Главная</a></li>
+        <li><a href="#" className="hover:text-black text-[#76767A]">Объявления</a></li>
+        <li><a href="#" className="hover:text-black text-[#76767A]">Афиша</a></li>
+        <li><a href="#" className="hover:text-black text-[#76767A]">Транспорт</a></li>
+        <li><a href="#" className="hover:text-black text-[#76767A]">Форум</a></li>
+        <li><a href="#" className="hover:text-black text-[#76767A]">Администрация</a></li>
+        <li><a href="#" className="hover:text-black text-[#76767A]">Архив</a></li>
       </ul>
     </nav>
   </div>
