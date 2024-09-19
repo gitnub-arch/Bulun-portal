a2bc8c820e3a524a799dd93ccdb1cd9899b4686e
Narizizu <tobokelovna1n@gmail.com>
2024-09-19 18:46:06 +0600

    added a mobile version

--- src/shared/footer/Footer.tsx
+++ src/shared/footer/Footer.tsx
@@ -1,26 +1,33 @@
 const Footer = () => {
-    return (
-      <div>
-        <footer className="bg-white p-4 text-black">
-  <div className="max-w-7xl mx-auto flex justify-between items-center">
-    <nav>
-      <ul className="flex space-x-4 gap-4">
-        <li className="text-black font-bold ml-56">Булунский портал</li>
-        <li><a href="#" className="hover:text-black text-[#76767A] ">Новости</a></li>
-        <li><a href="#" className="hover:text-black text-[#76767A]">Главная</a></li>
-        <li><a href="#" className="hover:text-black text-[#76767A]">Объявления</a></li>
-        <li><a href="#" className="hover:text-black text-[#76767A]">Афиша</a></li>
-        <li><a href="#" className="hover:text-black text-[#76767A]">Транспорт</a></li>
-        <li><a href="#" className="hover:text-black text-[#76767A]">Форум</a></li>
-        <li><a href="#" className="hover:text-black text-[#76767A]">Администрация</a></li>
-        <li><a href="#" className="hover:text-black text-[#76767A]">Архив</a></li>
-      </ul>
-    </nav>
-  </div>
-</footer>
-      </div>
-    );
-  };
-  
-  export default Footer;
-  
+  return (
+    <div>
+      <footer className="bg-white p-4 text-black">
+        <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center">
+          <ul className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 gap-4 text-center">
+            <li className="text-black font-bold ml-0 md:ml-56">Булунский портал</li>
+            <li>
+              <a href="#" className="hover:text-black text-[#76767A]">Новости</a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-black text-[#76767A]">Главная</a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-black text-[#76767A]">Объявления</a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-black text-[#76767A]">Транспорт</a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-black text-[#76767A]">Администрация</a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-black text-[#76767A]">Архив</a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
