ef438bec979075ef3eec1c0b083603500372d2fc
Namelesss01 <zajloobekovali00@gmail.com>
2024-09-20 15:46:10 +0600

    123

--- src/shared/footer/Footer.tsx
+++ src/shared/footer/Footer.tsx
@@ -3,25 +3,39 @@ const Footer = () => {
     <div>
       <footer className="bg-white p-4 text-black">
         <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center">
-          <ul className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 gap-4 text-center">
-            <li className="text-black font-bold ml-0 md:ml-56">Булунский портал</li>
-            <li>
-              <a href="#" className="hover:text-black text-[#76767A]">Новости</a>
+          <ul className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 gap-10 text-center">
+            <li className="text-black font-black text-center ml-16 mr-20 md:ml-28">
+              Булунский портал
             </li>
             <li>
-              <a href="#" className="hover:text-black text-[#76767A]">Главная</a>
+              <a href="#" className="hover:text-black text-[#76767A]">
+                Новости
+              </a>
             </li>
             <li>
-              <a href="#" className="hover:text-black text-[#76767A]">Объявления</a>
+              <a href="#" className="hover:text-black text-[#76767A]">
+                Главная
+              </a>
             </li>
             <li>
-              <a href="#" className="hover:text-black text-[#76767A]">Транспорт</a>
+              <a href="#" className="hover:text-black text-[#76767A]">
+                Объявления
+              </a>
             </li>
             <li>
-              <a href="#" className="hover:text-black text-[#76767A]">Администрация</a>
+              <a href="#" className="hover:text-black text-[#76767A]">
+                Транспорт
+              </a>
             </li>
             <li>
-              <a href="#" className="hover:text-black text-[#76767A]">Архив</a>
+              <a href="#" className="hover:text-black text-[#76767A]">
+                Администрация
+              </a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-black text-[#76767A]">
+                Архив
+              </a>
             </li>
           </ul>
         </div>
