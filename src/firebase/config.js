19aa0f2bb0b692efc0ddf74c124c7939e60e4046
Kostenko Oleg <ok609035@gmail.com>
2024-09-30 00:40:27 +0600

    import firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,13 +15,12 @@ const firebaseConfig = {
   projectId: "bulun-portal",
   storageBucket: "bulun-portal.appspot.com",
   messagingSenderId: "206699661294",
-  appId: "1:206699661294:web:8bb2b4d312a3a8ebdbf0ff",
-  measurementId: "G-CJST977FY4"
+  appId: "1:206699661294:web:52f11e6e86166563dbf0ff",
+  measurementId: "G-3BWXN3FSQM"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 const db = getFirestore(); // Инициализация базы данных Firestore
 const auth = getAuth(app); // Инициализация системы верификации
