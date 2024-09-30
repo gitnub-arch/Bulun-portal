f63743cfb951c5403aa0677eab0026efd97f7d19
Namelesss01 <zajloobekovali00@gmail.com>
2024-09-30 18:22:42 +0600

    323

--- src/pages/search_results/SearchResults.tsx
+++ src/pages/search_results/SearchResults.tsx
@@ -6,13 +6,15 @@ import { SEARCH_ITEM } from "./const";
 
 const SearchResults = () => {
   return (
-    <div className="flex mx-auto max-w-[1200px]">
-      <div>
-        <div className="flex mb-5">
-          <h2 className="text-2xl font-normal">Результат поиска:</h2>
-          <h1 className="font-semibold text-2xl">Вот то что вы искали</h1>
+    <div className="flex flex-col lg:flex-row mx-auto max-w-[1200px]">
+      <div className="w-full lg:w-3/4">
+        <div className="flex flex-col lg:flex-row lg:mb-5 mb-3">
+          <h2 className="text-xl lg:text-2xl font-normal">Результат поиска:</h2>
+          <h1 className="font-semibold text-xl lg:text-2xl lg:ml-4">
+            Вот то что вы искали
+          </h1>
         </div>
-        <div className="flex-wrap grid grid-cols-3 gap-5">
+        <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-5">
           {SEARCH_ITEM.map((item) => (
             <div className="rounded-[6px] bg-[#FFFFFF] max-w-[263px] min-h-[317px]">
               <img src={item.imageUrl} alt="" className="mb-4" />
@@ -29,7 +31,7 @@ const SearchResults = () => {
           ))}
         </div>
       </div>
-      <div>
+      <div className="hidden lg:block lg:w-1/4 lg:ml-6">
         <WeatherAside />
         <RoadstatusAside />
         <FlightsAside />
--- src/shared/footer/Footer.tsx
+++ src/shared/footer/Footer.tsx
@@ -1,41 +1,52 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <div>
-      <footer className="bg-white p-4 mt-14 text-black">
+      <footer className="bg-white p-4 mt-14 text-black mb-10">
         <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center">
           <ul className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4 gap-10 text-center">
             <li className="text-black font-black text-center ml-16 mr-20 md:ml-28">
               Булунский портал
             </li>
             <li>
-              <a href="#" className="hover:text-black text-[#76767A]">
-                Новости
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-black text-[#76767A]">
+              <Link to="/" className="hover:text-black text-[#76767A]">
                 Главная
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="#" className="hover:text-black text-[#76767A]">
+              <Link to="/news" className="hover:text-black text-[#76767A]">
+                Новости
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/announcement"
+                className="hover:text-black text-[#76767A]"
+              >
                 Объявления
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="#" className="hover:text-black text-[#76767A]">
+              <Link to="/transport" className="hover:text-black text-[#76767A]">
                 Транспорт
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="#" className="hover:text-black text-[#76767A]">
+              <Link
+                to="/administration"
+                className="hover:text-black text-[#76767A]"
+              >
                 Администрация
-              </a>
+              </Link>
             </li>
             <li>
-              <a href="#" className="hover:text-black text-[#76767A]">
+              <Link
+                to="/archive-page"
+                className="hover:text-black text-[#76767A]"
+              >
                 Архив
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
