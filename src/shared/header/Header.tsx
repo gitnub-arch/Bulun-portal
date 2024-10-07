584e1e052beb488670a66376e8c40ac64fae29a2
Kostenko Oleg <ok609035@gmail.com>
2024-10-07 17:36:17 +0600

    fix authtorization

--- src/shared/header/Header.tsx
+++ src/shared/header/Header.tsx
@@ -3,24 +3,24 @@ import {
   ChevronDown,
   CloudRainWind,
   Search,
-  User,
   X,
 } from "lucide-react";
 import { Separator } from "../../components/ui/separator";
-
 import { Link } from "react-router-dom";
 import { LINKS_ITEM } from "./const";
 import { useState, useEffect } from "react";
 import LinkItemProps from "./type";
 import Breadcrumbs from "../breadcrumbs/Breadcrumbs";
 import { useLocation, useNavigate } from "react-router-dom";
-import { Auth } from "../authorization/Auth"; // Компонент авторизации
+import { Auth } from "../authorization/Auth";
+import useAuthState from "../../hooks/useAuthState";
 
 const Header = () => {
+  const { isLoggedIn, isCheckingStatus } = useAuthState();
   const [activeLink, setActiveLink] = useState<LinkItemProps>(LINKS_ITEM[0]);
   const [history, setHistory] = useState<LinkItemProps[]>([LINKS_ITEM[0]]);
   const [isSearchActive, setIsSearchActive] = useState(false);
-  const [isMenuOpen, setIsMenuOpen] = useState(false); // состояние для открытия/закрытия бургер-меню
+  const [isMenuOpen, setIsMenuOpen] = useState(false); 
   const [searchQuery, setSearchQuery] = useState("");
   const navigate = useNavigate();
   const location = useLocation();
@@ -39,7 +39,7 @@ const Header = () => {
 
   const handleLinkClick = (link: LinkItemProps) => {
     updateActiveLink(link);
-    setIsMenuOpen(false); // Закрываем меню при выборе ссылки
+    setIsMenuOpen(false);
   };
 
   useEffect(() => {
@@ -51,9 +51,7 @@ const Header = () => {
     }
   }, [location]);
 
-  // Функция для перехода на страницу авторизации
-
-  // Обработчик нажатия клавиши Enter в поисковой строке
+  
   const handleSearchKeyDown = (e) => {
     if (e.key === "Enter" && searchQuery.trim()) {
       navigate(
@@ -85,13 +83,29 @@ const Header = () => {
               Булунский Портал
             </h1>
           </div>
-          <Auth>
-            <span className="ml-5 font-medium text-base text-[#999999] cursor-pointer">
-              Войти
-            </span>
-          </Auth>
+
+          {!isCheckingStatus && (
+            <>
+              {isLoggedIn ? (
+                <Link
+                  to="/account"
+                  className="ml-5 font-medium text-base text-[#999999]"
+                >
+                  Личный кабинет
+                </Link>
+              ) : (
+                <Auth>
+                  <span className="ml-5 font-medium text-base text-[#999999] cursor-pointer">
+                    Войти
+                  </span>
+                </Auth>
+              )}
+            </>
+          )}
         </div>
+
         <Separator className="bg-[#DADADA] mt-[26px] mb-[26px] w-full" />
+
         <div className="flex justify-between items-center mx-8">
           <div className="flex items-center">
             {/* Бургер-меню */}
@@ -108,14 +122,12 @@ const Header = () => {
                 />
               )}
             </div>
-            {/* Вертикальный разделитель */}
             <Separator
               orientation="vertical"
               className="bg-[#DADADA] -mt-6 ml-7 hidden lg:block"
             />
           </div>
 
-          {/* Основная навигация для десктопов */}
           <div className="hidden lg:flex justify-center items-center flex-grow gap-9">
             {!isSearchActive ? (
               <div className="flex justify-center gap-9 w-full">
@@ -135,16 +147,15 @@ const Header = () => {
               </div>
             ) : (
               <div className="relative w-full">
-                <div className="flex items-center border border-[#DADADA] rounded-md px-4 py-2 w-full mb-5">
-                  <Search className="text-[#999999] mr-2 block" />
+                <div className="flex items-center border border-[#DADADA] rounded-md px-4 py-2 lg:w-full mb-5">
+                  <Search className="text-[#999999] mr-2 " />
                   <input
                     type="text"
                     value={searchQuery}
                     onChange={(e) => setSearchQuery(e.target.value)}
-                    onKeyDown={handleSearchKeyDown} // Обработчик нажатия клавиши Enter
+                    onKeyDown={handleSearchKeyDown} 
                     placeholder="Введите фразу для поиска"
-                    className="flex-grow text-base outline-none 
-                 w-full sm:w-1/2 md:w-1/3 lg:w-1/4" // адаптивная ширина
+                    className="flex-grow outline-none text-sm sm:text-base"
                   />
                   <X
                     className="text-[#999999] cursor-pointer"
@@ -156,17 +167,16 @@ const Header = () => {
           </div>
 
           <div className="flex items-center">
-            {/* Вертикальный разделитель */}
             <Separator
               orientation="vertical"
               className="bg-[#DADADA] -mt-6 mr-7 hidden lg:block"
             />
             {!isSearchActive && (
               <Search
-                className="text-[#999999] cursor-pointer block -mt-5"
+                className="text-[#999999] cursor-pointer hidden lg:block -mt-5"
                 onClick={() => {
-                  setIsSearchActive(true); // Активируем поиск
-                  setSearchQuery(""); // Очищаем поле поиска
+                  setIsSearchActive(true);
+                  setSearchQuery("");
                 }}
               />
             )}
@@ -174,7 +184,6 @@ const Header = () => {
         </div>
       </div>
 
-      {/* Мобильное бургер-меню */}
       {isMenuOpen && (
         <div className="lg:hidden bg-white shadow-lg rounded-md p-4 absolute w-full top-0 left-0 z-50">
           <div className="flex justify-between items-center mb-3">
@@ -191,14 +200,26 @@ const Header = () => {
               {link.label}
             </a>
           ))}
-          {/* Войти в мобильной версии */}
-          <Auth>
-            <div className="flex mt-4">
-              <span className="ml-5 font-medium text-base text-[#999999] cursor-pointer">
-                Войти
-              </span>
-            </div>
-          </Auth>
+          {!isCheckingStatus && (
+            <>
+              {isLoggedIn ? (
+                <Link
+                  to="/account"
+                  className="ml-5 font-medium text-base text-[#999999]"
+                >
+                  Личный кабинет
+                </Link>
+              ) : (
+                <Auth>
+                  <div className="flex mt-4">
+                    <span className="ml-5 font-medium text-base text-[#999999] cursor-pointer">
+                      Войти
+                    </span>
+                  </div>
+                </Auth>
+              )}
+            </>
+          )}
         </div>
       )}
 
